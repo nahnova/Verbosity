7523211cfb7a34b840816dc9b12622af6bb78b5a
xavier1803 <56248472+xavier1803@users.noreply.github.com>
2023-04-03 10:37:15 +0200

    New front end
    New front end based on the Feedback system assignment.

---
+++ FeedbackSysteem/FeedbackSysteem/Form1.cs
@@ -0,0 +1,21 @@
+namespace FeedbackSysteem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegisterForm registerForm = new RegisterForm();
+            registerForm.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Home home = new Home();
+            home.ShowDialog();
+        }
+    }
+}
---
+++ FeedbackSysteem/FeedbackSysteem/GoalForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace FeedbackSysteem
+{
+    partial class GoalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(388, 40);
+            button3.Name = "button3";
+            button3.Size = new Size(134, 29);
+            button3.TabIndex = 6;
+            button3.Text = "Delete goal";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(248, 40);
+            button2.Name = "button2";
+            button2.Size = new Size(134, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Update goal";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(108, 40);
+            button1.Name = "button1";
+            button1.Size = new Size(134, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Add goal";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(27, 88);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(556, 387);
+            listView1.TabIndex = 7;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // GoalForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(623, 487);
+            Controls.Add(listView1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "GoalForm";
+            Text = "GoalForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private ListView listView1;
+    }
+}
