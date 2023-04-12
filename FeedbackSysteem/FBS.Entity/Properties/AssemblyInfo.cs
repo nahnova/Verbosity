952767e44e89574638e26833631ecdc444799d5b
Nah-Nova <noaheutz@hotmail.nl>
2023-04-12 22:09:44 +0200

    Changes IMS to FBS troughout entire application

--- FeedbackSysteem/FBS.Entity/Properties/AssemblyInfo.cs
+++ FeedbackSysteem/FBS.Entity/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IMS.Entity")]
+[assembly: AssemblyTitle("FBS.Entity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IMS.Entity")]
+[assembly: AssemblyProduct("FBS.Entity")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- FeedbackSysteem/FBS.Repository/Properties/AssemblyInfo.cs
+++ FeedbackSysteem/FBS.Repository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IMS.Repository")]
+[assembly: AssemblyTitle("FBS.Repository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IMS.Repository")]
+[assembly: AssemblyProduct("FBS.Repository")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
